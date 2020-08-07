4d5007ff7fcc49c303502ca7d9a7c0aca2ab8883
tomornw <tomornw@mitrphol.com>
2020-08-07 16:45:13 +0700

    add test case

--- spring_restful/src/test/java/th/in/tomorn/springjpa/SpringjpaApplicationTests.java
+++ spring_restful/src/test/java/th/in/tomorn/springjpa/SpringjpaApplicationTests.java
@@ -1,5 +1,7 @@
 package th.in.tomorn.springjpa;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class SpringjpaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void queryGradeBP() {
+		String dataString = "A";
+		assertEquals("A", dataString);
 	}
 
 }
